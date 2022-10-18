1f8e1b5f56f30a3c5e5f6312bc1f6ecb6d427547
Ivan Molotkov <ivan.molotkov@start-i.ru>
2022-10-18 16:42:46 +0300

    Changed calculator interface communication

--- __main__.py
+++ __main__.py
@@ -1,29 +1,40 @@
 import sys
 import time
+import typing as t
+from dataclasses import dataclass
 from functools import reduce
 from operator import mul
-
+from enum import Enum, auto
 from solver import (
     Cube, rotate, generate_cube, CubeAddError,
     get_all_cubes, get_all_rotates, cube_rotation, print_c
 )
-from data import figures
+from data import figures as figures_
 
 
-class TimeSplitter:
-    def __init__(self, delta=0.01):
-        self.time = time.time()
-        self.delta = delta
-
-    def __bool__(self):
-        time_ = time.time()
-        if time_ - self.time > self.delta:
-            self.time = time_
-            return True
-        return False
+class MsgTypes(Enum):
+    VARIANTS = auto()
+    PROGRESS = auto()
+    FOUNDED = auto()
+    END = auto()
+    TOTAL = auto()
 
 
-if __name__ == '__main__':
+@dataclass
+class Msg:
+    type: MsgTypes
+    payload: t.Any | None
+
+    def __iter__(self):
+        return iter((self.type, self.payload))
+
+
+def calculate(figures, consumer):
+
+    def send(msg_type, msg):
+        msg = Msg(msg_type, msg)
+        consumer(msg)
+
     base, *others = figures
 
     e_cube = Cube(generate_cube(3))
@@ -39,8 +50,6 @@ if __name__ == '__main__':
             rotated = cube_rotation(base_c, rotations)
             cbs.add(rotated)
 
-    base_cubes = base_cubes_
-
     cubes_set = []
     for fig in others:
         current_set = set()
@@ -54,40 +63,75 @@ if __name__ == '__main__':
     common_time = time.time()
     cubes_set.insert(0, base_cubes_)
 
-    completed = []
+    count = 0
+    counts = [len(c_set) for c_set in cubes_set] + [1]
+    all_solutions = reduce(mul, counts)
+    sols_c = [reduce(mul, counts[cp:]) for cp in range(len(counts))]
+
+    send(MsgTypes.VARIANTS, all_solutions)
 
     tasks = [(e_cube, cube, 0) for cube in cubes_set[0]]
-
-    counts = [len(c_set) for c_set in cubes_set] + [1]
-    print('Полученные размеры:', *counts)
-    count = 0
-
-    all_solutions = reduce(mul, counts)
-    sols_c = [
-      reduce(mul, counts[cp:]) for cp in range(len(counts))
-    ]
-    print('Всего вариантов: ', all_solutions)
-
     while tasks:
         cube, new_cube, pos = tasks.pop()
 
         try:
             pos += 1
             cube = cube + new_cube
-            time.sleep(0.01)
+            time.sleep(0.001)
             if pos == len(figures):
-                completed.append(cube)
+                send(MsgTypes.FOUNDED, cube)
                 count += 1
                 continue
             tasks.extend((cube, new_cube, pos) for new_cube in cubes_set[pos])
-            progress = int(count / all_solutions * 50)
-            sys.stdout.write(f"\rПрогресс: ·{'>' * progress}{' ' * (50 - progress)}·")
-            sys.stdout.flush()
+            send(MsgTypes.PROGRESS, count)
         except CubeAddError:
             count += sols_c[pos]
 
-    print(f"\rПрогресс: ·{'>' * 50}·")
-    print(f'Затрачено {time.time() - common_time:.3} sec')
-    print("\nНайдено решение:\n")
-    for res in completed:
-        print_c(res)
+    send(MsgTypes.PROGRESS, count)
+    send(MsgTypes.TOTAL, time.time() - common_time)
+    send(MsgTypes.END, None)
+
+
+def consumer_():
+    count_variants = None
+    results = []
+    while True:
+        msgtype, value = yield
+        match msgtype:
+            case MsgTypes.VARIANTS:
+                count_variants = value
+                print(f'Всего вариантов: {value}')
+                continue
+
+            case MsgTypes.PROGRESS:
+                progress = int(value / count_variants * 50)
+                sys.stdout.write(f"\rПрогресс: ·{'>' * progress}{' ' * (50 - progress)}·")
+                sys.stdout.flush()
+                continue
+
+            case MsgTypes.FOUNDED:
+                results.append(value)
+                continue
+
+            case MsgTypes.TOTAL:
+                print(f'\nЗатрачено {value:.3} sec')
+                continue
+
+            case MsgTypes.END:
+                for res in results:
+                    print("\nНайдено решение:\n")
+                    print_c(res)
+                return
+
+
+def main():
+    try:
+        cons = consumer_()
+        next(cons)
+        calculate(figures_, cons.send)
+    except StopIteration:
+        pass
+
+
+if __name__ == '__main__':
+    main()
